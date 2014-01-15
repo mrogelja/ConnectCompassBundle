1b418c973e5747260b86f280c47ec7aaf7c0509e
mrogelja <mathieu.rogelja@gmail.com>
2014-01-15 14:36:50 +0100

    Revert deleted files.

---
+++ MrConnectCompassBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Copyright 2012 Ina Studio Hypermedia
+ */
+
+namespace Mr\ConnectCompassBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * MrConnectCompassBundle.
+ */
+class MrConnectCompassBundle extends Bundle
+{
+}
